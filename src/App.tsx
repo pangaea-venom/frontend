639828c2049a78450c3fc962081d6773ebda2b50
Chavo Kim <68835703+chavokim@users.noreply.github.com>
2023-06-10 16:18:41 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -49,7 +49,7 @@ function App() {
 
         setLoading(true)
 
-        const amount = toNano(21.1)
+        const amount = toNano(1.1)
 
         try {
             await daoContract.methods.joinDao({ name: username }).send({
